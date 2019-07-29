8a7fb1e034824180a7bf17712865a431678390fa
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-29 16:20:25 +1000

    Override resource budget with custom values.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-build-an-operator
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-build-an-operator:master
-RESOURCE_BUDGET=medium
+RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=14400
 IDLE_TIMEOUT=1800
