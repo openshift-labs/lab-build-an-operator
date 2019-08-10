64c9947171e124e315d8811b7490f5cf62045568
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-08-10 17:10:16 +1000

    Update to latest workshop scripts with workshop memory setting.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,6 @@
 WORKSHOP_NAME=lab-build-an-operator
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-build-an-operator:master
+WORKSHOP_MEMORY=2Gi
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=14400
 IDLE_TIMEOUT=1800
