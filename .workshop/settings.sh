6ad001c34a6b71d69f6444f560e4ffc45455e5d5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-08-11 10:40:30 +1000

    Override default role for hub to be cluster admin through spawner template.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,6 @@
 WORKSHOP_NAME=lab-build-an-operator
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-build-an-operator:master
+SPAWNER_ROLE=cluster-admin
 WORKSHOP_MEMORY=2Gi
 RESOURCE_BUDGET=custom
 MAX_SESSION_AGE=14400
