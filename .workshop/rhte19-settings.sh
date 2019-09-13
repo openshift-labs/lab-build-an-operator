86e49f10bfaa7faf6ba7a510eebbf2ba127a09f5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-09-13 20:55:05 +1000

    Update image version for rhte19.

--- .workshop/rhte19-settings.sh
+++ .workshop/rhte19-settings.sh
@@ -1,4 +1,4 @@
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-build-an-operator:rhte19-2.1
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-build-an-operator:rhte19-2.2
 PREPULL_IMAGES=true
 CLEAN_INSTALL=true
 SERVER_LIMIT=150
